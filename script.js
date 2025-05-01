43486f56ffd6044879ad0dd37b37073c8c537e2f
DeannaWallace05 <DeannaWallace2024@afscholars.org>
2025-05-01 11:58:06 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+let optionOne=document.querySelector(".option-one")
+let optionTwo=document.querySelector(".option-two")
+let story=document.querySelector(".story-opening")
+let option1=document.querySelector(".option-one-screen")
+let 
+option2=document.querySelector(".option-two-screen")
+ let 
+optionEnding=document.querySelector("option-one-end")  
+ let 
+optionEnding2=document.querySelector(".option-two-end")
+ let 
+screen3=document.querySelector("option-one-end");  
+ let 
+screen4=document.querySelector(".option-two-end")
+
+
+  // When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+
+// Example for transitioning to the next page (level)
+function () {
+    window.location.href = "level2.html"; // Replace with the next level's URL
+}
+
